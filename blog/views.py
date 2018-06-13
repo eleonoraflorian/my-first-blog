1214605da797165bf9a8cf4652c065f9f07768f7
prova <prova@prova.com>
2018-06-13 09:55:59 +0200

    Non so cosa sto facendo

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
